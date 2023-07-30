08239f4f190d96c289e1a1956ffe2b2240e2f5b8
Youssef0Eldeeb <youssefeldeeb3109@gmail.com>
2023-07-30 16:42:17 +0300

    Initial Commit

---
+++ AnimatedApp/AnimatedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedAppApp.swift
+//  AnimatedApp
+//
+//  Created by Youssef Eldeeb on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
